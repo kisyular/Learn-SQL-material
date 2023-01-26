7cb41582ac6affc0d1004a65b63a1aca03ff5580
kisyular <rellika@kisyula.com>
2023-01-26 14:44:03 +0300

    QUIZ - TO UNDERSTAND JOINS

--- SQL-Notes.sql
+++ SQL-Notes.sql
@@ -901,7 +901,161 @@ in the customer and order columns.
 */
 
 
+/*
+QUIZ
+*/
 
+-- Create Country table with countryID, countryName columns.
+CREATE TABLE Country
+(
+    countryID   INT,
+    countryName VARCHAR(50)
+);
+-- Insert data into Country table.
+INSERT INTO Country (countryID, countryName)
+VALUES (1, 'India'),
+       (2, 'Nepal'),
+       (3, 'United States'),
+       (4, 'Canada'),
+       (5, 'Sri Lanka'),
+       (6, 'Brazil');
+
+-- Create State table with stateID, stateName, countryID columns.
+CREATE TABLE State
+(
+    stateID   INT,
+    countryID INT,
+    stateName VARCHAR(50)
+);
+-- Insert data into State table.
+INSERT INTO State (stateID, countryID, stateName)
+VALUES (1, 1, 'Maharashtra'),
+       (2, 1, 'Punjab'),
+       (3, 2, 'Kathmandu'),
+       (4, 3, 'California'),
+       (5, 3, 'Texas'),
+       (6, 4, 'Alberta');
+
+-- Select countryID, countryName and stateName from Country and State tables.
+SELECT Country.countryID, Country.countryName, State.stateName
+FROM Country
+         JOIN State
+              ON Country.countryID = State.countryID;
+
+-- Select countryID, countryName and stateName from Country and State tables using LEFT JOIN.
+SELECT Country.countryID, Country.countryName, State.stateName
+FROM Country
+         LEFT JOIN State
+                   ON Country.countryID = State.countryID;
+
+-- Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for
+-- the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the
+-- account name. Sort the accounts alphabetically (A-Z) according to account name.
+
+SELECT region.name, sales_reps.name, accounts.name
+FROM region
+         JOIN sales_reps
+              ON region.id = sales_reps.region_id
+         JOIN accounts
+              ON sales_reps.id = accounts.sales_rep_id
+WHERE region.name = 'Midwest'
+ORDER BY accounts.name;
+
+-- Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for
+-- accounts where the sales rep has a first name starting with S and in the Midwest region. Your final table should
+-- include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically
+-- (A-Z) according to account name.
+
+SELECT region.name, sales_reps.name, accounts.name
+FROM region
+         JOIN sales_reps
+              ON region.id = sales_reps.region_id
+         JOIN accounts
+              ON sales_reps.id = accounts.sales_rep_id
+WHERE region.name = 'Midwest'
+  AND sales_reps.name LIKE 'S%'
+ORDER BY accounts.name;
+
+-- Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for
+-- accounts where the sales rep has a last name starting with K and in the Midwest region. Your final table should
+-- include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically
+-- (A-Z) according to account name.
+
+SELECT region.name, sales_reps.name, accounts.name
+FROM region
+         JOIN sales_reps
+              ON region.id = sales_reps.region_id
+         JOIN accounts
+              ON sales_reps.id = accounts.sales_rep_id
+WHERE region.name = 'Midwest'
+  AND sales_reps.name LIKE '% K%'
+ORDER BY accounts.name;
+
+-- Provide the name for each region for every order, as well as the account name and the unit price they paid
+-- (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity
+-- exceeds 100. Your final table should have 3 columns: region name, account name, and unit price. In order to avoid
+-- a division by zero error, adding .01 to the denominator here is helpful total_amt_usd/(total+0.01).
+
+SELECT region.name, accounts.name, total_amt_usd / (total + 0.01) AS unit_price
+FROM region
+         JOIN sales_reps
+              ON region.id = sales_reps.region_id
+         JOIN accounts
+              ON sales_reps.id = accounts.sales_rep_id
+         JOIN orders
+              ON accounts.id = orders.account_id
+WHERE orders.standard_qty > 100;
+
+-- Provide the name for each region for every order, as well as the account name and the unit price they paid
+-- (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity
+-- exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account
+-- name, and unit price. Sort for the smallest unit price first. In order to avoid a division by zero error, adding .01
+-- to the denominator here is helpful (total_amt_usd/(total+0.01).
+
+SELECT region.name, accounts.name, total_amt_usd / (total + 0.01) AS unit_price
+FROM region
+         JOIN sales_reps
+              ON region.id = sales_reps.region_id
+         JOIN accounts
+              ON sales_reps.id = accounts.sales_rep_id
+         JOIN orders
+              ON accounts.id = orders.account_id
+WHERE orders.standard_qty > 100
+  AND orders.poster_qty > 50
+ORDER BY unit_price;
+
+-- What are the different channels used by account id 1001? Your final table should have only 2 columns: account name
+-- and the different channels. You can try SELECT DISTINCT to narrow down the results to only the unique values.
+
+SELECT DISTINCT accounts.name, web_events.channel
+FROM accounts
+         JOIN web_events
+              ON accounts.id = web_events.account_id
+WHERE accounts.id = 1001;
+
+/*
+The DISTINCT keyword is used to return only distinct (different) values. The result set will not contain any duplicate
+values. The DISTINCT keyword is used with the SELECT statement.
+
+The following SQL statement selects all the different channels used by account id 1001:
+SELECT DISTINCT accounts.name, web_events.channel
+FROM accounts
+JOIN web_events
+ON accounts.id = web_events.account_id
+WHERE accounts.id = 1001;
+*/
+
+
+-- Find all the orders that occurred in 2015. Your final table should have 4 columns: occurred_at, account name, order
+-- total, and order total_amt_usd.
+-- Hint: You can use the EXTRACT function to extract the year from the occurred_at column.
+-- EXTRACT(YEAR FROM occurred_at) AS year
+
+SELECT orders.occurred_at, accounts.name, orders.total, orders.total_amt_usd
+FROM accounts
+         JOIN orders
+              ON accounts.id = orders.account_id
+WHERE EXTRACT(YEAR FROM orders.occurred_at) = 2015;
 
 
 
