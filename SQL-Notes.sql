61dc34c2161b177b1de53295696ae714b56eb491
kisyular <rellika@kisyula.com>
2023-01-26 16:53:03 +0300

    GROUP BY CLAUSE

--- SQL-Notes.sql
+++ SQL-Notes.sql
@@ -1270,14 +1270,57 @@ GROUP BY web_events.channel, accounts.name
 ORDER BY web_events.channel, accounts.name;
 
 
+/*
+ QUIZ
+ */
+
+ -- For each account, determine the average amount of each type of paper they purchased across their orders. Your
+ -- result should have four columns - one for the account name and one for the average quantity purchased for each
+ -- of the paper types for each account.
+SELECT accounts.name, AVG(orders.standard_qty) as avg_standard_qty, AVG(orders.gloss_qty) as avg_gloss_qty,
+       AVG(orders.poster_qty) as avg_poster_qty
+FROM accounts
+         JOIN orders
+              ON accounts.id = orders.account_id
+GROUP BY accounts.name;
 
 
+-- For each account, determine the average amount spent per order on each paper type. Your result should have four
+-- columns - one for the account name and one for the average amount spent on each paper type.
+SELECT accounts.name, AVG(orders.standard_amt_usd) as avg_standard_amt_usd,
+       AVG(orders.gloss_amt_usd) as avg_gloss_amt_usd,
+       AVG(orders.poster_amt_usd) as avg_poster_amt_usd
+FROM accounts
+         JOIN orders
+              ON accounts.id = orders.account_id
+GROUP BY accounts.name;
 
 
+-- Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final
+-- table should have three columns - the name of the sales rep, the channel, and the number of occurrences.
+-- Order your table with the highest number of occurrences first.
+SELECT sales_reps.name, web_events.channel, COUNT(web_events.id) as total_web_events
+FROM web_events
+         JOIN accounts
+              ON web_events.account_id = accounts.id
+         JOIN sales_reps
+              ON accounts.sales_rep_id = sales_reps.id
+GROUP BY sales_reps.name, web_events.channel
+ORDER BY total_web_events DESC;
 
 
-
-
-
+-- Determine the number of times a particular channel was used in the web_events table for each region. Your final
+-- table should have three columns - the region name, the channel, and the number of occurrences. Order your table
+-- with the highest number of occurrences first.
+SELECT region.name, web_events.channel, COUNT(web_events.id) as total_web_events
+FROM accounts
+         JOIN web_events
+              ON accounts.id = web_events.account_id
+         JOIN sales_reps
+                ON accounts.sales_rep_id = sales_reps.id
+            JOIN region
+                ON sales_reps.region_id = region.id
+GROUP BY region.name, web_events.channel
+ORDER BY total_web_events DESC;
 
 
