cab62abd7d0eb4f0458191184092e9b9bd367149
kisyular <rellika@kisyula.com>
2023-01-26 14:14:12 +0300

    LIKE, IN, NOT, AND & BETWEEN, OR CLAUSE

--- SQL-Notes.sql
+++ SQL-Notes.sql
@@ -251,4 +251,118 @@ FROM orders
 LIMIT 10;
 
 
+/*
+LIKE
+LIKE This allows you to perform operations similar to using WHERE and =, but for cases when you might not know
+exactly what you are looking for.
+
+IN This allows you to perform operations similar to using WHERE and =, but for more than one condition.
+NOT This is used with IN and LIKE to select all of the rows NOT LIKE or NOT IN a certain condition.
+AND & BETWEEN These allow you to combine operations where all combined conditions must be true.
+OR This allows you to combine operations where at least one of the combined conditions must be true.
+*/
+
+-- All the companies whose names start with 'C'.
+SELECT *
+FROM accounts
+WHERE name LIKE 'C%'
+LIMIT 10;
+
+-- All companies whose names contain the string 'one' somewhere in the name.
+SELECT *
+FROM accounts
+WHERE name LIKE '%one%';
+
+-- All companies whose names end with 's'.
+SELECT *
+FROM accounts
+WHERE name LIKE '%s'
+LIMIT 10;
+
+-- Use the accounts table to find the account name, primary_poc, and sales_rep_id for Walmart, Target, and Nordstrom.
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+-- Use the web_events table to find all information regarding individuals who were contacted via the channel of organic
+-- or adwords.
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords')
+LIMIT 5;
+
+-- Use the accounts table to find the account name, primary poc, and sales rep id for all stores except Walmart, Target,
+-- and Nordstrom.
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
+
+-- Use the web_events table to find all information regarding individuals who were contacted via any method except using
+-- organic or adwords methods.
+SELECT *
+FROM web_events
+WHERE channel NOT IN ('organic', 'adwords');
+
+
+-- All the companies whose names do not start with 'C'.
+SELECT *
+FROM accounts
+WHERE name NOT LIKE 'C%';
+
+
+-- All companies whose names do not contain the string 'one' somewhere in the name.
+SELECT *
+FROM accounts
+WHERE name NOT LIKE '%one%';
+
+-- All companies whose names do not end with 's'.
+SELECT *
+FROM accounts
+WHERE name NOT LIKE '%s';
+
+
+-- Write a query that returns all the orders where the standard_qty is over 1000, the poster_qty is 0, and the
+-- gloss_qty is 0.
+SELECT *
+FROM orders
+WHERE standard_qty > 1000
+  AND poster_qty = 0
+  AND gloss_qty = 0;
+
+-- Using the accounts table, find all the companies whose names do not start with 'C' and end with 's'.
+SELECT *
+FROM accounts
+WHERE name NOT LIKE 'C%'
+  AND name NOT LIKE '%s';
+
+-- Use the web_events table to find all information regarding individuals who were contacted via the organic or adwords
+-- channels, and started their account at any point in 2016, sorted from newest to oldest.
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords')
+  AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01'
+ORDER BY occurred_at DESC;
+
+-- Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. Only include the id field in the
+-- resulting table.
+SELECT id
+FROM orders
+WHERE gloss_qty > 4000
+   OR poster_qty > 4000;
+
+-- Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is
+-- over 1000.
+SELECT *
+FROM orders
+WHERE standard_qty = 0
+  AND (gloss_qty > 1000 OR poster_qty > 1000);
+
+-- Find all the company names that start with a 'C' or 'W', and the primary contact contains 'ana' or 'Ana', but
+-- it doesn't contain 'eana'.
+SELECT name, primary_poc
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%')
+  AND (primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%' OR primary_poc NOT LIKE '%eana%');
+
+
 
