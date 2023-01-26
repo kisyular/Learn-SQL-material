b2e9d35c0235dd63469036df0f2eaba9215d8653
kisyular <rellika@kisyula.com>
2023-01-26 14:22:59 +0300

    JOIN

--- SQL-Notes.sql
+++ SQL-Notes.sql
@@ -455,6 +455,84 @@ Then in the ON, we will ALWAYS have the PK equal to the FK:
 */
 
 
+/*
+JOIN
+-- A JOIN clause is used to combine rows from two or more tables, based on a related column between them.
+-- The most common type of JOIN is the INNER JOIN, which returns all rows when there is at least one match in both
+tables.
+-- The following are the different types of JOINs:
+    -- INNER JOIN: Returns all rows when there is at least one match in both tables
+    -- LEFT JOIN: Returns all rows from the left table, and the matched rows from the right table
+    -- RIGHT JOIN: Returns all rows from the right table, and the matched rows from the left table
+    -- FULL JOIN: Returns all rows when there is a match in one of the tables
+*/
+
+/*
+INNER JOIN
+-- The INNER JOIN keyword selects all rows from both tables as long as there is a match between the columns in both
+tables.
+-- The following statement selects all orders and the account name for each order from the orders and accounts
+tables.
+*/
+
+-- Try pulling all the data from the accounts table, and all the data from the orders table.
+SELECT orders.*, accounts.*
+FROM accounts
+         JOIN orders ON accounts.id = orders.account_id;
+
+
+-- Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and then website and the primary_poc
+-- from the accounts table.
+SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.website, accounts.primary_poc
+FROM accounts
+         JOIN orders ON accounts.id = orders.account_id;
+
+
+-- pull data from web_events, accounts, and orders tables using a JOIN.
+SELECT web_events.*, accounts.*, orders.*
+FROM web_events
+         JOIN accounts ON web_events.account_id = accounts.id
+         JOIN orders ON accounts.id = orders.account_id;
+
+-- pull data from web_events, accounts, and orders tables using a JOIN using ALIAS a for accounts, w for web_events and
+-- o for orders.
+SELECT w.*, a.*, o.*
+FROM web_events AS w
+         JOIN accounts AS a ON w.account_id = a.id
+         JOIN orders AS o ON a.id = o.account_id;
+
+-- Provide a table for all web_events associated with account name of Walmart. There should be three columns. Be sure
+-- to include the primary_poc, time of the event, and the channel for each event. Additionally, you might choose to add
+-- a fourth column to assure only Walmart events were chosen.
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+FROM web_events AS w
+         JOIN accounts AS a ON w.account_id = a.id
+WHERE a.name = 'Walmart';
+
+
+-- Provide a table that provides the region for each sales_rep along with their associated accounts. Your final table
+-- should include three columns: the region name, the sales rep name, and the account name. Sort the accounts
+-- alphabetically (A-Z) according to account name.
+SELECT r.name region, s.name rep, a.name account
+FROM sales_reps s
+         JOIN region r
+              ON s.region_id = r.id
+         JOIN accounts a
+              ON a.sales_rep_id = s.id
+ORDER BY a.name;
+
+-- Provide the name for each region for every order, as well as the account name and the unit price they paid
+-- (total_amt_usd/total) for the order. Your final table should have 3 columns: region name, account name, and unit
+-- price. A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.
+SELECT r.name region, a.name account, o.total_amt_usd / (o.total + 0.01) unit_price
+FROM region r
+         JOIN sales_reps s
+              ON s.region_id = r.id
+         JOIN accounts a
+              ON a.sales_rep_id = s.id
+         JOIN orders o
+              ON o.account_id = a.id
+ORDER BY r.name DESC;
 
 
 
