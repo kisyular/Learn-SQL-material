d172256e005e0c174693d725faf2848974d0873f
kisyular <rellika@kisyula.com>
2023-01-27 16:38:11 +0300

    CASE STATEMENT

--- SQL-Notes.sql
+++ SQL-Notes.sql
@@ -1611,5 +1611,129 @@ ORDER BY total_sales DESC
 LIMIT 1;
 
 
+/*
+CASE STATEMENTS
+-------------------------------------------------------------------------------------------------------------------
+PostgreSQL provides a CASE statement for performing conditional logic. The CASE statement is similar to an IF statement
+in other programming languages. The CASE statement is used to create a new column based on the values of existing
+columns. The CASE statement is also used to filter rows based on the values of existing columns.
+
+CASE WHEN condition THEN result
+     WHEN condition THEN result
+     ELSE result
+END
+
+- The CASE statement always goes in the SELECT clause.
+- CASE must include the following components: WHEN, THEN, and END. ELSE is an optional component to catch cases that
+  did not meet any of the other previous CASE conditions.
+- You can make any conditional statement using any conditional operator (like WHERE) between WHEN and THEN.
+  This includes stringing together multiple conditional statements using AND and OR.
+- You can include multiple WHEN statements, as well as an ELSE statement again, to deal with any unaddressed conditions.
+*/
+
+-- Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for
+-- each order. Limit the results to the first 10 orders, and include the id and account_id fields.
+/*
+NOTE - you will be thrown an error with the correct solution to this question. This is for a division by zero.
+You will learn how to get a solution without an error to this query when you learn about CASE statements in a later
+section.
+*/
+-- USE CASE STATEMENT TO FIX DIVISION BY ZERO ERROR
+SELECT account_id, CASE WHEN standard_qty = 0 OR standard_qty IS NULL THEN 0
+                        ELSE standard_amt_usd / standard_qty
+                   END AS unit_price
+FROM orders
+LIMIT 10;
+
+
+-- Write a query to display for each order, the account ID, total amount of the order, and the level of the order -
+-- ‘Large’ or ’Small’ - depending on if the order is $3000 or more, or smaller than $3000.
+SELECT account_id, total_amt_usd,
+       CASE WHEN total_amt_usd >= 3000 THEN 'Large'
+            ELSE 'Small'
+       END AS order_level
+FROM orders
+LIMIT 15;
+
+-- Write a query to display the number of orders in each of three categories, based on the total number of items in
+-- each order. The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.
+SELECT CASE WHEN total >= 2000 THEN 'At Least 2000'
+            WHEN total >= 1000 THEN 'Between 1000 and 2000'
+            ELSE 'Less than 1000'
+       END AS order_level,
+       COUNT(id) AS num_orders
+FROM orders
+GROUP BY order_level;
+
+-- We would like to understand 3 different branches of customers based on the amount associated with their purchases.
+-- The top branch includes anyone with a Lifetime Value (total sales of all orders) greater than 200,000 usd.
+-- The second branch is between 200,000 and 100,000 usd. The lowest branch is anyone under 100,000 usd. Provide a
+-- table that includes the level associated with each account. You should provide the account name, the total sales
+-- of all orders for the customer, and the level. Order with the top spending customers listed first.
+SELECT accounts.name, SUM(total_amt_usd) AS total_sales,
+       CASE WHEN SUM(total_amt_usd) > 200000 THEN 'Top'
+            WHEN SUM(total_amt_usd) > 100000 THEN 'Middle'
+            ELSE 'Low'
+       END AS level
+FROM orders
+         JOIN accounts
+              ON orders.account_id = accounts.id
+GROUP BY accounts.name
+ORDER BY total_sales DESC;
+
+-- We would now like to perform a similar calculation to the first, but we want to obtain the total amount spent by
+-- customers only in 2016 and 2017. Keep the same levels as in the previous question. Order with the top spending
+-- customers listed first.
+SELECT accounts.name, SUM(total_amt_usd) AS total_sales,
+       CASE WHEN SUM(total_amt_usd) > 200000 THEN 'Top'
+            WHEN SUM(total_amt_usd) > 100000 THEN 'Middle'
+            ELSE 'Low'
+       END AS level
+FROM orders
+         JOIN accounts
+              ON orders.account_id = accounts.id
+WHERE occurred_at BETWEEN '2016-01-01' AND '2017-12-31'
+GROUP BY accounts.name
+ORDER BY total_sales DESC;
+
+-- We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders.
+-- Create a table with the sales rep name, the total number of orders, and a column with top or not depending on if
+-- they have more than 200 orders. Place the top sales people first in your final table.
+SELECT sales_reps.name, COUNT(orders.id) AS num_orders,
+       CASE WHEN COUNT(orders.id) > 200 THEN 'Top'
+            ELSE 'Not Top'
+       END AS top
+FROM orders
+         JOIN accounts
+              ON orders.account_id = accounts.id
+            JOIN sales_reps
+              ON accounts.sales_rep_id = sales_reps.id
+GROUP BY sales_reps.name
+ORDER BY num_orders DESC;
+
+-- The previous didn't account for the middle, nor the dollar amount associated with the sales. Management decides they
+-- want to see these characteristics represented as well. We would like to identify top performing sales reps, which
+-- are sales reps associated with more than 200 orders or more than 750000 in total sales. The middle group has any rep
+-- with more than 150 orders or 500000 in sales. Create a table with the sales rep name, the total number of orders,
+-- total sales across all orders, and a column with top, middle, or low depending on this criteria. Place the top sales
+-- people based on dollar amount of sales first in your final table. You might see a few upset sales people by this
+-- criteria!
+SELECT sales_reps.name, COUNT(orders.id) AS num_orders, SUM(total_amt_usd) AS total_sales,
+       CASE WHEN COUNT(orders.id) > 200 OR SUM(total_amt_usd) > 750000 THEN 'Top'
+            WHEN COUNT(orders.id) > 150 OR SUM(total_amt_usd) > 500000 THEN 'Middle'
+            ELSE 'Low'
+       END AS level
+FROM orders
+         JOIN accounts
+              ON orders.account_id = accounts.id
+            JOIN sales_reps
+              ON accounts.sales_rep_id = sales_reps.id
+GROUP BY sales_reps.name
+ORDER BY total_sales DESC;
+
+
+
+
+
 
 
